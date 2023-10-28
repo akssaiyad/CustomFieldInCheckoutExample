a5ab3694ddabc8c9900bf21677a8b9db181b45a6
akssaiyad <akssaiyad@gmail.com>
2023-10-28 15:56:43 +0530

    CustomFieldInCheckout

---
+++ CustomFieldInCheckout/Block/Adminhtml/Order/View/DisplayCustomValue.php
@@ -0,0 +1,22 @@
+<?php
+namespace Aks\CustomFieldInCheckout\Block\Adminhtml\Order\View;
+
+use Magento\Sales\Api\Data\OrderInterface;
+
+class DisplayCustomValue extends \Magento\Backend\Block\Template
+{
+    public function __construct(
+        \Magento\Backend\Block\Widget\Context $context,
+        OrderInterface $orderInterface,
+        array $data = []
+    ) {
+        $this->orderInterface = $orderInterface;
+        parent::__construct($context, $data);
+    }
+    public function getAccept(){
+        $orderId = $this->getRequest()->getParam('order_id');
+        $order = $this->orderInterface->load($orderId);
+
+        return $order->getAccept();
+    }
+}
